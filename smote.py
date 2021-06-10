1a3dd5c8e34520a0386ca4c1261a8ffedec44bb5
Chloe <49031258+cpappas18@users.noreply.github.com>
2021-06-10 15:10:27 -0400

    Now supports multi-class one-hot encoded labels

--- smote.py
+++ smote.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 ## Synthetic Minority Oversampling Technique (SMOTE)
-This script demonstrates how to transform an imbalanced dataset (used for binary classification)
-into a balanced dataset by oversampling from the minority class.
+These functions transform an imbalanced dataset into a balanced dataset by using the SMOTE technique to
+oversample from the minority class(es).
 
 Reference: 
 https://machinelearningmastery.com/smote-oversampling-for-imbalanced-classification/
@@ -10,14 +10,14 @@ https://machinelearningmastery.com/smote-oversampling-for-imbalanced-classificat
 
 import pandas as pd
 import numpy as np
-import imblearn
 from random import random
 from collections import Counter
-from matplotlib import pyplot
-from numpy import where
 from imblearn.over_sampling import SMOTE
 
-def smote(df):
+"""
+Performs SMOTE on a dataframe with binary labels.
+"""
+def smote_binary(df):
   
   if df.columns[[0]] == 'Unnamed: 0':
     df.drop(df.columns[[0]], axis=1, inplace=True) # remove first column
@@ -26,25 +26,8 @@ def smote(df):
   x = df.drop(['Status', 'Gender'], axis=1) # features only, Gender also removed because it must be binary
   gender = df.Gender
 
-  counter = Counter(y) # {label_0: num_0, label_1: num_1}
-  if counter[0] < counter[1]:
-    minority = counter[0]
-    majority = counter[1]
-  else:
-    minority = counter[1]
-    majority = counter[0]
-
   oversample = SMOTE()
   x_oversampled, y_oversampled = oversample.fit_resample(x, y)
-
-  # fill gender column with random choice of 0 or 1
-  gender_oversampled = gender.to_list()
-  for _ in range(majority-minority):
-    rand = random()
-    if rand < 0.5:
-      gender_oversampled.append(0)
-    else:
-      gender_oversampled.append(1)
 
   # get sample counts for balanced dataset
   counter = Counter(y_oversampled) # {label_0: num_0, label_1: num_1}
@@ -54,7 +37,46 @@ def smote(df):
     print("Oversampling failed")
 
   # regenerate dataframe with new samples
-  df = pd.concat([pd.DataFrame(y_oversampled), pd.DataFrame(gender_oversampled), pd.DataFrame(x_oversampled)], axis=1)
-  df.columns = ['Status', 'Gender'] + x.columns.to_list()
+  df_concat = pd.concat([pd.DataFrame(y_oversampled), pd.DataFrame(gender), pd.DataFrame(x_oversampled)], axis=1)
+  df_concat.columns = df.columns
 
-  return df, count
+  # fill missing gender values with a random choice of 0 or 1
+  for row in df_concat.loc[df_concat.Gender.isnull(), 'Gender'].index:
+    df_concat.at[row, 'Gender'] = random.randint(0, 2)
+
+  return df_concat, count
+
+
+"""
+Performs SMOTE on a dataframe with multi-class one-hot encoded labels. 
+"""
+def smote_multiclass_encoded(df):
+
+  if df.columns[[0]] == 'Unnamed: 0':
+    df.drop(df.columns[[0]], axis=1, inplace=True)  # remove first column
+
+  y = df.values[:, 0:4]  # one-hot encoded labels
+  x = df.values[:, 5:]  # features only, Gender also removed because it must be binary
+  gender = df.Gender
+
+  oversample = SMOTE()
+  x_oversampled, y_oversampled = oversample.fit_resample(x, y)
+
+  numeric_y_os = [np.where(r == 1)[0][0] for r in y_oversampled]
+
+  # get sample counts for balanced dataset
+  counter = Counter(numeric_y_os)
+  if counter[0] == counter[1] and counter[1] == counter[2] and counter[2] == counter[3]:
+    count = counter[0]
+  else:
+    print("Oversampling failed")
+
+  # regenerate dataframe with new samples
+  df_concat = pd.concat([pd.DataFrame(y_oversampled), pd.DataFrame(gender), pd.DataFrame(x_oversampled)], axis=1)
+  df_concat.columns = df.columns
+
+  # fill missing gender values with a random choice of 0 or 1
+  for row in df_concat.loc[df_concat.Gender.isnull(), 'Gender'].index:
+    df_concat.at[row, 'Gender'] = random.randint(0, 2)
+
+  return df_concat, count
