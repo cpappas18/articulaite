b798d5df46d9482c553880912aa124728171ca39
Chloe Pappas <chloeoliviapappas@gmail.com>
2021-06-11 16:17:34 -0400

    Now supports undersampling and numerical class labels

--- smote.py
+++ smote.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-## Synthetic Minority Oversampling Technique (SMOTE)
-These functions transform an imbalanced dataset into a balanced dataset by using the SMOTE technique to
-oversample from the minority class(es).
-
 Reference: 
 https://machinelearningmastery.com/smote-oversampling-for-imbalanced-classification/
 """
@@ -15,28 +11,53 @@ from imblearn.over_sampling import SMOTE
 from numpy import where, random
 
 
-"""
-Performs SMOTE on a dataframe with binary labels.
-"""
-def smote_binary(df):
-  
+def smote_binary(df, num_samples_dict=None):
+  """
+  Performs data augmentation and/or downsampling on a dataframe with binary classes.
+  The SMOTE algorithm is used for augmentation and random sampling is used for downsampling of the specified classes.
+
+  Keyword arguments:
+  df -- the dataframe to manipulate
+  num_samples_dict -- a dictionary of the form {0: num_desired_samples, 1: num_desired_samples} that indicates the
+                      number of samples that the returned dataframe should have in each class
+  """
+
   if df.columns[[0]] == 'Unnamed: 0':
     df.drop(df.columns[[0]], axis=1, inplace=True) # remove first column
+
+  if num_samples_dict is not None:
+    strategy = num_samples_dict
+
+    # downsampling classes if needed
+    counter = Counter(df.Status)
+    class_samples = []
+    for i in range(2):
+      if counter[i] > num_samples_dict[i]: # class i needs to be downsampled
+        class_i = df.loc[df['Status'] == i]
+        class_i_sample = class_i.sample(n=num_samples_dict[i])
+        class_samples.append(class_i_sample)
+      else: # class i does not need downsampling
+        class_i = df.loc[df['Status'] == i]
+        class_samples.append(class_i)
+
+    df = pd.concat(class_samples).reset_index(drop=True)
+
+  else:
+    strategy = 'auto'
 
   y = df.Status # labels
   x = df.drop(['Status', 'Gender'], axis=1) # features only, Gender also removed because it must be binary
   gender = df.Gender
 
-  oversample = SMOTE()
+  oversample = SMOTE(sampling_strategy=strategy)
   x_oversampled, y_oversampled = oversample.fit_resample(x, y)
 
-  # get sample counts for balanced dataset
-  counter = Counter(y_oversampled) # {label_0: num_0, label_1: num_1}
-  if counter[0] == counter[1]: 
-    count = counter[0]
-  else:
-    count = 0
-    print("Oversampling failed")
+  # get sample counts for augmented dataset
+  counter = Counter(y_oversampled) # {0: num_samples, 1: num_samples}
+  if num_samples_dict is not None and (counter[0] != num_samples_dict[0] or counter[1] != num_samples_dict[1]):
+    print("Sampling failed")
+  elif num_samples_dict is None and counter[0] != counter[1]:
+    print("Sampling failed")
 
   # regenerate dataframe with new samples
   df_concat = pd.concat([pd.DataFrame(y_oversampled), pd.DataFrame(gender), pd.DataFrame(x_oversampled)], axis=1)
@@ -46,33 +67,77 @@ def smote_binary(df):
   for row in df_concat.loc[df_concat.Gender.isnull(), 'Gender'].index:
     df_concat.at[row, 'Gender'] = random.randint(0, 2)
 
-  return df_concat, count
+  return df_concat
 
 
-"""
-Performs SMOTE on a dataframe with multi-class one-hot encoded labels. 
-"""
-def smote_multiclass_encoded(df):
+
+def smote_multiclass(df, num_samples_dict=None, one_hot_encoded=False):
+  """
+  Performs data augmentation and/or downsampling on a dataframe with multiple classes.
+  Supports both numerical and one-hot encoded class values.
+  The SMOTE algorithm is used for augmentation and random sampling is used for downsampling of the specified classes.
+
+  Keyword arguments:
+  df -- the dataframe to manipulate
+  num_samples_dict -- a dictionary of the form {0: num_desired_samples, 1: num_desired_samples, ...} that indicates the
+                      number of samples that the returned dataframe should have in each class
+  one_hot_encoded -- Boolean value indicating if the dataframe is one-hot encoded
+  """
 
   if df.columns[[0]] == 'Unnamed: 0':
     df.drop(df.columns[[0]], axis=1, inplace=True)  # remove first column
 
-  y = df.values[:, 0:4]  # one-hot encoded labels
-  x = df.values[:, 5:]  # features only, Gender also removed because it must be binary
+  # change one hot encoded dataset into numerical labels
+  if one_hot_encoded:
+    df.insert(loc=0, column='Status', value=np.nan)
+    for i, row in df.iterrows():
+      if df.iloc[i]['Control'] == 1:
+          status = 0
+      elif df.iloc[i]['Parkinsons'] == 1:
+          status = 1
+      elif df.iloc[i]['ALS'] == 1:
+          status = 2
+      elif df.iloc[i]['Cerebral_palsy'] == 1:
+          status = 3
+      df.at[i,'Status'] = status
+    df = df.drop(columns=['Control', 'Parkinsons', 'ALS', 'Cerebral_palsy'])
+
+
+  if num_samples_dict is not None:
+    strategy = num_samples_dict
+
+    # downsampling classes if needed
+    counter = Counter(df.Status)
+    class_samples = []
+    for i in range(4):
+      if counter[i] > num_samples_dict[i]: # class i needs to be downsampled
+        class_i = df.loc[df['Status'] == i]
+        class_i_sample = class_i.sample(n=num_samples_dict[i])
+        class_samples.append(class_i_sample)
+      else: # class i does not need downsampling
+        class_i = df.loc[df['Status'] == i]
+        class_samples.append(class_i)
+
+    df = pd.concat(class_samples).reset_index(drop=True)
+
+  else:
+    strategy = 'auto'
+
+  y = df.Status # labels
+  x = df.drop(['Status', 'Gender'], axis=1) # features only, Gender also removed because it must be binary
   gender = df.Gender
 
-  oversample = SMOTE()
+  oversample = SMOTE(sampling_strategy=strategy)
   x_oversampled, y_oversampled = oversample.fit_resample(x, y)
 
-  numeric_y_os = [np.where(r == 1)[0][0] for r in y_oversampled]
+  # get sample counts for augmented dataset
+  counter = Counter(y_oversampled)
 
-  # get sample counts for balanced dataset
-  counter = Counter(numeric_y_os)
-  if counter[0] == counter[1] and counter[1] == counter[2] and counter[2] == counter[3]:
-    count = counter[0]
-  else:
-    count = 0
-    print("Oversampling failed")
+  if num_samples_dict is not None and (counter[0] != num_samples_dict[0] or counter[1] != num_samples_dict[1]
+                                       or counter[2] != num_samples_dict[2] or counter[3] != num_samples_dict[3]):
+    print("Sampling failed")
+  elif num_samples_dict is None and (counter[0] != counter[1] or counter[1] != counter[2] or counter[2] != counter[3]):
+    print("Sampling failed")
 
   # regenerate dataframe with new samples
   df_concat = pd.concat([pd.DataFrame(y_oversampled), pd.DataFrame(gender), pd.DataFrame(x_oversampled)], axis=1)
@@ -82,4 +147,11 @@ def smote_multiclass_encoded(df):
   for row in df_concat.loc[df_concat.Gender.isnull(), 'Gender'].index:
     df_concat.at[row, 'Gender'] = random.randint(0, 2)
 
-  return df_concat, count
+  # change numerical class values back into one-hot encoded values
+  if one_hot_encoded:
+    one_hot = pd.get_dummies(df_concat['Status'])
+    one_hot.rename(columns={0:'Control', 1:'Parkinsons', 2:'ALS', 3:'Cerebral_palsy'}, inplace=True)
+    df_concat = df_concat.drop(columns=['Status'], axis=1)
+    df_concat = pd.concat([one_hot, df_concat], axis=1)
+
+  return df_concat
